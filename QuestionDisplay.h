b48917db3e93ecd7caf639d6515fbec7d4ecae84
Matthew Ziesing <mattzinger852@gmail.com>
2024-10-01 18:33:08 +0930

    first creation of the QuestionDisplay class

---
+++ QuestionDisplay.h
@@ -0,0 +1,20 @@
+// creating a header protection for the class
+#ifndef QUESTIONDISPLAY_H
+#define QUESTIONDISPLAY_H
+
+#include <iostream>
+
+// constructing the QuestionDisplay class
+class QuestionDisplay {
+private:
+    std::string userAnswer; // represents the user's given answer
+
+public:
+    QuestionDisplay(); // default constructor for the QuestionDisplay class
+
+    virtual void questionDisplay() = 0; // abstract class to create an interface to display the different questions
+    std::string get_Answer(); // returns the user's given answer
+    void set_Answer(std::string answer); // sets the inputted answer as the user's given answer
+} ;
+
+#endif
