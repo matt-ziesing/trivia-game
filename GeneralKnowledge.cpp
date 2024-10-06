9bdb0a17d6de15adbc89c16cea8b2623b4439c2b
Matthew Ziesing <mattzinger852@gmail.com>
2024-10-06 12:51:38 +1030

    added the general knowledge class, and created a solution for it in the main file to be called by the abstract class

---
+++ GeneralKnowledge.cpp
@@ -0,0 +1,84 @@
+#include "GeneralKnowledge.h"
+using namespace std;
+
+GeneralKnowledge::GeneralKnowledge() {
+    // creates an array to store all the questions
+    questions = new string[5]{};
+    max_questions = 5;
+
+    // these are the questions that will be accessed by the player for the history category
+    questions[0] = "What did the iconic 'Red Skin' lolly change their name to in 2020?";
+    questions[1] = "What is tallest mountain on Earth?";
+    questions[2] = "What is the name of the all-time highest grossing film?";
+    questions[3] = "According to the Guiness World Records, what is the best-selling book of all time?";
+    questions[4] = "What is the collective noun for a group of crows?";
+}
+
+void GeneralKnowledge::questionDisplay() {
+
+    // this is how the questions will be displayed to the user using the abstract class
+    if (get_chosenQuestion() == 1) {
+        cout << questions[0] << endl;
+
+    } else if (get_chosenQuestion() == 2) {
+        cout << questions[1] << endl;
+
+    } else if (get_chosenQuestion() == 3) {
+        cout << questions[2] << endl;
+
+    } else if (get_chosenQuestion() == 4) {
+        cout << questions[3] << endl;
+
+    } else if (get_chosenQuestion() == 5) {
+        cout << questions[4] << endl;
+        
+    }
+}
+
+void GeneralKnowledge::questionDelete() {
+    // this determines what question will not be copied over, and be deleted
+    int questionDeleteIndex = get_chosenQuestion();
+    int copyIndex = 0;
+
+    // checks to see if there is still at least 1 question left in the category
+    if ((max_questions - 1) != 0) {
+        string* tempArray = new string[max_questions]{};
+
+        // copies and pastes over only the questions not marked for deletion
+        for (int i = 0; i < max_questions; i++) {
+            int paste_index = i;
+            if (i != questionDeleteIndex) {
+                tempArray[paste_index] = questions[copyIndex];
+                copyIndex++;
+                paste_index++;
+
+            } else {
+                copyIndex++;
+            }
+        }
+        // dereferences the temp array to be the questions array, and decrements the number of max questions
+        max_questions--;
+        set_generalKnowledgeLength(get_generalKnowledgeLength() - 1);
+        questions = tempArray;
+    }
+}
+
+string* GeneralKnowledge::get_Questions() {
+    return questions; // returns the array of questions in the category
+}
+
+int GeneralKnowledge::get_maxQuestions() {
+    return max_questions; // returns the max_number of questions in the category
+}
+
+void GeneralKnowledge::set_Questions(string* questions) {
+    GeneralKnowledge::questions = questions; // sets the given array of questions to be the contained array of questions
+}
+
+void GeneralKnowledge::set_maxQuestions(int max_questions) {
+    GeneralKnowledge::max_questions = max_questions; // sets the max number of questions to be the given int
+}
+
+GeneralKnowledge::~GeneralKnowledge() {
+    delete[] questions; // deallocates the space used for the questions array
+}
--- makefile
+++ makefile
@@ -3,3 +3,6 @@ h-test:	History.cpp CategoryPicker.cpp QuestionDisplay.cpp historyTest-main.cpp
 
 vg-test: History.cpp VideoGames.cpp CategoryPicker.cpp QuestionDisplay.cpp vidgameTest-main.cpp
 	g++ -Wall -o vdt VideoGames.cpp History.cpp CategoryPicker.cpp QuestionDisplay.cpp vidgameTest-main.cpp
+
+gk-test: History.cpp VideoGames.cpp GeneralKnowledge.cpp CategoryPicker.cpp QuestionDisplay.cpp genKnowledgeTest-main.cpp
+	g++ -Wall -o gkt History.cpp VideoGames.cpp GeneralKnowledge.cpp CategoryPicker.cpp QuestionDisplay.cpp genKnowledgeTest-main.cpp
